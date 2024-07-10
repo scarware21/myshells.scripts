e4173a535c144c8a7534ee78e01f1e0b0e5ab4b9
scarware21 <157497769+scarware21@users.noreply.github.com>
2024-07-09 21:27:59 -0300

    Add files via upload
    verificador ip externo

---
+++ ipexterno.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+#####verificando ip externo
+
+
+ip_externo=$(curl -s icanhazip.com)
+
+                                          \
+				           \
+echo "Seu ip externo é: $ip_externo"
+
+echo "########################"
+
+
